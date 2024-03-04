802bafb84225f2be6d701a723446c6ef750f073b
kadoiOS <160595235+kadoiOS@users.noreply.github.com>
2024-03-04 12:41:10 +0300

    Add files via upload

---
+++ 3.py
@@ -0,0 +1,12 @@
+# Kullanıcıdan alınan 3 sayının en büyüğünü bulan programı yazınız ?
+
+
+a = int(input("a sayısı: "))
+
+b = int(input("b sayısı: "))
+
+c = int(input("c sayısı: "))
+
+buyuk = max(a,b,c)
+
+print(buyuk)
